9bf2d1acdf84614e28dab9d00cc04664143aa67f
Yunhao <yz2327@cornell.edu>
2020-11-02 10:37:33 -0500

    add deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+if [ "$#" -le 0 ]
+then
+
+    echo "Usage: deploy.sh {username}"
+
+else
+    # remove old log and data
+    rm ./deploy/*/log/*
+    rm ./deploy/*/data/*
+    
+    cat hosts | while read machine
+    do
+        echo "deploy code on machine ${machine} at ${POMPE_HOME}"
+
+        rsync -rtuv ./libhotstuff/examples $1@${machine}:${POMPE_HOME}/libhotstuff/ 
+
+	    rsync -rtuv ./libhotstuff/deploy $1@${machine}:${POMPE_HOME}/libhotstuff/
+
+    done
+
+fi
---
+++ install_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install libssl-dev libuv1-dev dh-autoreconf cmake make
+
+mkdir ~/pompe
+
+mkdir ~/pompe/libhotstuff
